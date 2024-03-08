d54131dc5e4cc72b437e764261276b58189402b8
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-08 15:03:45 +0600

    rendering arrays with map

--- react-practice-app/src/Singers.jsx
+++
@@ -1,9 +0,0 @@
-export default function Singers({ singer }) {
-   const { name, age } = singer;
-   return (
-      <div>
-         <h3>Singer Name: {name}</h3>
-         <p>Singer Age: {age}</p>
-      </div>
-   );
-}
